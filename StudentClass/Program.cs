d66d5ecec0cf686fc8d5e83b3a4714ba1ebc60f3
max-student <max-student@maxtrain.com>
2020-06-18 15:35:42 -0400

    Add project files.

---
+++ StudentClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StudentClass {
+    class Program {
+        static void Main(string[] args) {
+
+
+            var jada = new Student();
+            jada.Name = "Jada";
+            jada.Id = 1;
+            jada.GPA = 3.5;
+            jada.SAT = 1600;
+            jada.TimesTardy = 112;
+            Console.WriteLine($"Jada's name is {jada.Name} ");
+
+            var steve = new Student();
+            steve.Name = "Steve";
+            steve.Id = 2;
+            steve.SAT = 1599;
+            steve.GPA = 4.0;
+            steve.TimesTardy = 0;
+            var steveString = steve.GetValues();
+            Console.WriteLine($"Steve: {steveString}");
+            Console.WriteLine($"Steve's BC score is {steve.BC(2)}");
+
+            var dave = new Student("Dave", 3, 1, 1600, 3.8) ;
+            var matt = new Student("Matt", 4, 2, 1598, 3.9) ;
+            var jon = new Student("Dave", 5, 3, 1597, 3.7) ;
+        }
+    }
+}
---
+++ StudentClass/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentClass {
+
+    class Student {
+        public double GPA;
+        public int SAT;
+        public int TimesTardy;
+        public string Name;
+        public int Id;
+
+        private double SecretNumberCalculation(double bnumber, int SecretNumber) {
+            return bnumber / SecretNumber;
+        }
+
+        public double BC(int SecretNumber) {
+            var dblSat = SAT * 2;
+            var b = dblSat * GPA;
+            var bc = b / SecretNumberCalculation(b, SecretNumber);
+            return bc;
+        }
+
+        public string GetValues() {
+            return $" ID: {Id}|NAME: {Name}|TIMES TARDY: {TimesTardy}|SAT: {SAT}|GPA: {GPA}";
+
+        }
+
+        public Student() { // default constructor
+        
+        }
+        public Student(string Name, int Id, int TimesTardy, int SAT, double GPA) {
+            this.Name = Name;
+            this.Id = Id;
+            this.TimesTardy = TimesTardy;
+            this.SAT = SAT;
+            this.GPA = GPA;
+       
+        }   
+
+
+
+    } 
+}
